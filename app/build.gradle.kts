d365412a48ff333019b718959eebd574bc5eef4a
hostei <hostei@qq.com>
2026-04-13 09:38:48 +0800

    禁用 Release 混淆和资源压缩

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = true
-            isShrinkResources = true
+            isMinifyEnabled = false
+            isShrinkResources = false
 //            isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
